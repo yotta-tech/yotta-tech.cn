146b90f78f67a5312d1b525320d70177f57936ed
jackgit <ygjack414@hotmail.com>
2017-05-09 16:26:07 +0800

    'init'

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="container">
+    <img class="logo" src="~assets/yotta.jpg">
+    <p><a href="yotta-tech.cn">yotta-tech.cn</a> is under construction</p>
+    <p>email: gangyang@yotta-tech.cn</p>
+    <p>mobile: 186-2783-4950</p>
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.logo {
+  width: 100px;
+  height: 100px;
+  margin-bottom: 40px;
+}
+</style>
